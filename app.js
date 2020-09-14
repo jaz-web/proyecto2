11c3bf402dd4a3dda5df3a902c59faa562c633d7
jaz-web <71184736+jaz-web@users.noreply.github.com>
2020-09-14 19:47:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Registering Service Worker
 /*if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js');
+	navigator.serviceWorker.register('/proyecto2/sw.js');
 };*/
 
 //****************
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('/proyecto2/sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
